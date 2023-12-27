d8d357acca51f9d111da8c8ac9537874db80124e
RicardoSolanoPereira <corretorsolano@gmail.com>
2023-12-27 15:02:35 -0300

    Criando testes

---
+++ main.py
@@ -0,0 +1,12 @@
+from calculadora import soma
+
+# print(soma(10, 20))
+# print(soma(-10, 20))
+# print(soma(10.5, 20.5))
+
+try:
+    print(soma('15', 15))
+except AssertionError as e:
+    print(f'Conta inválida: {e}')
+
+print('Conta', soma(25, 25))
